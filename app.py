41a6b3a27f293ecdbbd2ba62e068a7265c81d6c9
sainath-zessta <sainath.sapa@zessta.com>
2023-12-21 12:31:24 +0530

    completed CRUD with Flask and MongoDB

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import exception
 from flask_mongoengine import MongoEngine
 from flask import request, session, redirect, url_for
 from flask import Flask, jsonify
@@ -25,32 +26,32 @@ class UserModel(db.Document):
     This class represents a user in the system.
 
     Attributes:
-        _id (str): The unique ID of the user.
+        id (str): The unique ID of the user.
         userName (str): The username of the user.
         password (str): The password of the user.
 
     Methods:
-        delete(self, _id): This method deletes a user from the system.
+        delete(self, id): This method deletes a user from the system.
         to_json(self): This method returns a JSON representation of the object.
         getID(self): This method returns the ID of the user.
     """
 
-    _id = db.StringField(primary_key=True)
+    # id = db.StringField(primary_key=True, required=False)
     userName = db.StringField(required=True, unique=True)
     password = db.StringField(required=True)
 
-    def delete(self, _id):
+    def delete(self, id):
         """
         This method deletes a user from the system.
 
         Args:
-            _id (str): The ID of the user to be deleted.
+            id (str): The ID of the user to be deleted.
 
         Returns:
             list: A list containing the deleted user.
         """
         userList = []
-        userList = list(filter(lambda x: x["_id"] != _id, userList))
+        userList = list(filter(lambda x: x["id"] != id, userList))
         return userList + "deleted"
 
     def to_json(self):
@@ -61,7 +62,7 @@ class UserModel(db.Document):
             dict: A JSON representation of the object.
         """
         return {
-            "_id": self._id,
+            "id": self.id,
             "userName": self.userName,
         }
 
@@ -72,18 +73,20 @@ class UserModel(db.Document):
         Returns:
             str: The ID of the user.
         """
-        return self._id
+        return self.id
+
 
 """
 DB Model for Personal Details
 """
+
 
 class PersonalDetailsModel(db.Document):
     """
     This class represents the personal details of a user.
 
     Attributes:
-        _id (str): The unique ID of the user.
+        id (str): The unique ID of the user.
         userID (ObjectId): The ID of the user.
         firstName (str): The first name of the user.
         lastName (str): The last name of the user.
@@ -93,11 +96,11 @@ class PersonalDetailsModel(db.Document):
         gender (str): The gender of the user.
 
     Methods:
-        delete(self, _id): This method deletes a personal details from the system.
+        delete(self, id): This method deletes a personal details from the system.
         to_json(self): This method returns a JSON representation of the object.
     """
 
-    _id = db.StringField(primary_key=True)
+    # id = db.StringField(primary_key=True)
     userID = db.ObjectIdField(required=True)
     firstName = db.StringField(default="N/A")
     lastName = db.StringField(default="N/A")
@@ -106,19 +109,19 @@ class PersonalDetailsModel(db.Document):
     address = db.StringField(default="N/A")
     gender = db.StringField(default="N/A")
 
-    def delete(self, _id):
+    def delete(self, id):
         """
         This method deletes a personal details from the system.
 
         Args:
-            _id (str): The ID of the personal details to be deleted.
+            id (str): The ID of the personal details to be deleted.
 
         Returns:
             list: A list containing the deleted personal details.
         """
         PersonalDetailsList = []
         PersonalDetailsList = list(
-            filter(lambda x: x["_id"] != _id, PersonalDetailsList)
+            filter(lambda x: x["id"] != id, PersonalDetailsList)
         )
         return PersonalDetailsList + "deleted"
 
@@ -130,7 +133,7 @@ class PersonalDetailsModel(db.Document):
             dict: A JSON representation of the object.
         """
         return {
-            "_id": self._id,
+            "id": self.id,
             "userID": self.userID,
             "firstName": self.firstName,
             "lastName": self.lastName,
@@ -145,12 +148,13 @@ class PersonalDetailsModel(db.Document):
 Employees Model
 """
 
+
 class EmployeesModel(db.Document):
     """
     This class represents the employees details of a user.
 
     Attributes:
-        _id (str): The unique ID of the user.
+        id (str): The unique ID of the user.
         userID (ObjectId): The ID of the user.
         employeeName (str): The name of the employee.
         workEmail (str): The work email of the employee.
@@ -160,11 +164,11 @@ class EmployeesModel(db.Document):
         department (str): The department of the employee.
 
     Methods:
-        delete(self, _id): This method deletes an employees details from the system.
+        delete(self, id): This method deletes an employees details from the system.
         to_json(self): This method returns a JSON representation of the object.
     """
 
-    _id = db.StringField(primary_key=True)
+    # id = db.StringField(primary_key=True)
     userID = db.ObjectIdField(required=True)
     employeeName = db.StringField(default="N/A")
     workEmail = db.StringField(default="N/A")
@@ -173,18 +177,18 @@ class EmployeesModel(db.Document):
     designation = db.StringField(default="N/A")
     department = db.StringField(default="N/A")
 
-    def delete(self, _id):
+    def delete(self, id):
         """
         This method deletes an employees details from the system.
 
         Args:
-            _id (str): The ID of the employees details to be deleted.
+            id (str): The ID of the employees details to be deleted.
 
         Returns:
             list: A list containing the deleted employees details.
         """
         EmployeesList = []
-        EmployeesList = list(filter(lambda x: x["_id"] != _id, EmployeesList))
+        EmployeesList = list(filter(lambda x: x["id"] != id, EmployeesList))
         return EmployeesList + "deleted"
 
     def to_json(self):
@@ -195,7 +199,7 @@ class EmployeesModel(db.Document):
             dict: A JSON representation of the object.
         """
         return {
-            "_id": self._id,
+            "id": self.id,
             "userID": self.userID,
             "employeeName": self.employeeName,
             "workEmail": self.workEmail,
@@ -204,6 +208,7 @@ class EmployeesModel(db.Document):
             "designation": self.designation,
             "department": self.department,
         }
+
 
 """
 Model for RBAC
@@ -215,7 +220,7 @@ class RBACModel(db.Document):
     This class represents the role based access control details of a user.
 
     Attributes:
-        _id (str): The unique ID of the user.
+        id (str): The unique ID of the user.
         userID (ObjectId): The ID of the user.
         role (str): The role of the user.
 
@@ -224,7 +229,7 @@ class RBACModel(db.Document):
         to_json(self): This method returns a JSON representation of the object.
     """
 
-    _id = db.StringField(primary_key=True)
+    # id = db.StringField(primary_key=True)
     userID = db.ObjectIdField(required=True)
     role = db.StringField(default="N/A")
 
@@ -236,7 +241,7 @@ class RBACModel(db.Document):
             list: A list containing the deleted RBAC details.
         """
         RBACList = []
-        RBACList = list(filter(lambda x: x["_id"] != self._id, RBACList))
+        RBACList = list(filter(lambda x: x["id"] != self.id, RBACList))
         return RBACList + "deleted"
 
     def to_json(self):
@@ -247,10 +252,11 @@ class RBACModel(db.Document):
             dict: A JSON representation of the object.
         """
         return {
-            "_id": self._id,
+            "id": self.id,
             "userID": self.userID,
             "role": self.role,
         }
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -293,10 +299,11 @@ def index():
                         "workPhone",
                         "companyName",
                         "designation",
-                    ]
-                }
-            ]
-        })
+                    ],
+                },
+            ],
+        }
+    )
 
 
 @app.route("/register", methods=["POST"])
@@ -324,22 +331,6 @@ def register():
     """
     This function registers a new user in the system.
 
-    Args:
-        userName (str): The username of the user.
-        password (str): The password of the user.
-        firstName (str): The first name of the user.
-        lastName (str): The last name of the user.
-        email (str): The email ID of the user.
-        mobile (str): The mobile number of the user.
-        address (str): The address of the user.
-        gender (str): The gender of the user.
-        workEmail (str): The work email of the employee.
-        workPhone (str): The work phone of the employee.
-        companyName (str): The company name of the employee.
-        designation (str): The designation of the employee.
-        department (str): The department of the employee.
-        role (str): The role of the user.
-
     Returns:
         dict: A JSON object containing the message and status code.
     """
@@ -362,7 +353,7 @@ def register():
 
     # USERCREATIONLOGIC
     # Check if the user already exists
-    if UserModel.objects(userName=userName).count() >= 0:
+    if UserModel.objects(userName=userName).count() > 0:
         # If the user exists, return an error
         return jsonify({"message": "User Already Exists"})
     else:
@@ -408,9 +399,12 @@ def register():
             session["role"] = role
 
             # Return a success message
-            return jsonify({"message": "User Created and Session Started"})
-        except:
+            return jsonify(
+                {"message": "User Created and Session Started", "id": objectID}
+            )
+        except Exception as e:
             # If there is an error, return an error message
+            print(e)
             return jsonify({"message": "Something went wrong", "status": 500})
 
 
@@ -440,12 +434,14 @@ def login():
     userName = parseRequestParameters.get("userName")
     password = parseRequestParameters.get("password")
     # Check if the user exists in the database
-    tempUserObjFetch = UserModel.objects(userName=userName, password=password).count()
+    tempUserObjFetch = UserModel.objects(userName=userName, password=password)
 
-    if tempUserObjFetch > 0:
+    if tempUserObjFetch.count() > 0:
         # Create a session for the user
         session["userID"] = userName
-        return jsonify({"message": "Logged In Successfully"})
+        return jsonify(
+            {"message": "Logged In Successfully", "id": tempUserObjFetch.first().id}
+        )
     else:
         return jsonify({"message": "Invalid Credentials"})
 
@@ -461,11 +457,19 @@ def logout():
     Returns:
         dict: A JSON object containing the message and status code.
     """
-    session.clear()
-    return jsonify({"message": "Logged Out Successfully"})
+    if session.get("userID"):
+        session.clear()
+        return jsonify({"message": "Logged Out Successfully"})
+    else:
+        return jsonify({"Error": "Couldn't log out", "message": "User not logged in"})
 
 
-@app.route("/user/get", methods=GET_POST)
+def checkLogin():
+    if session.get("userID") == None:
+        return False
+
+
+@app.route("/user/get", methods=["GET"])
 # This function retrieves the user details based on the user ID.
 # It returns a JSON object containing the user details.
 def getUser():
@@ -479,45 +483,100 @@ def getUser():
     Returns:
         dict: A JSON object containing the user details.
     """
-    if session.get("userID") == None:
+    if checkLogin() == False:
         return jsonify({"ERROR": {"message": "Not Logged In"}})
-    else:
+
+    parseRequestParameters = request.values
+    obj = ObjectId(parseRequestParameters.get("id"))
+
+    tempUserObjFetch = UserModel.objects.filter(id=obj).first()
+
+    if tempUserObjFetch == None:
+        return jsonify({"message": "User Not Found"})
+    tempUserPersonalObjFetch = PersonalDetailsModel.objects.filter(userID=obj).first()
+    tempUserEmployeesObjFetch = EmployeesModel.objects.filter(userID=obj).first()
+    tempRoleObjFetch = RBACModel.objects.filter(userID=obj).first()
+
+    returnJsonFile = {
+        "userDetails": {"userName": tempUserObjFetch.userName},
+        "PersonalDetails": {
+            "firstName": tempUserPersonalObjFetch.firstName,
+            "lastName": tempUserPersonalObjFetch.lastName,
+            "email": tempUserPersonalObjFetch.email,
+            "mobile": tempUserPersonalObjFetch.mobile,
+            "address": tempUserPersonalObjFetch.address,
+            "gender": tempUserPersonalObjFetch.gender,
+        },
+        "CompanyDetails": {
+            "companyName": tempUserEmployeesObjFetch.companyName,
+            "designation": tempUserEmployeesObjFetch.designation,
+            "department": tempUserEmployeesObjFetch.department,
+            "workEmail": tempUserEmployeesObjFetch.workEmail,
+            "workPhone": tempUserEmployeesObjFetch.workPhone,
+            "role": tempRoleObjFetch.role,
+        },
+    }
+
+    return returnJsonFile
+
+
+@app.route("/user/update", methods=["PUT"])
+def updateUser():
+    if checkLogin() == False:
+        return jsonify({"ERROR": {"message": "Not Logged In"}})
+
+    try:
         parseRequestParameters = request.values
-        obj = ObjectId(parseRequestParameters.get("id"))
+        Objid = ObjectId(parseRequestParameters.get("id"))
 
-        tempUserObjFetch = UserModel.objects.filter(_id=obj).first()
+        requestJSONData = request.get_json()
 
-        if tempUserObjFetch == None:
-            return jsonify({"message": "User Not Found"})
-        tempUserPersonalObjFetch = PersonalDetailsModel.objects.filter(
-            userID=obj
-        ).first()
-        tempUserEmployeesObjFetch = EmployeesModel.objects.filter(
-            userID=obj
-        ).first()
-        tempRoleObjFetch = RBACModel.objects.filter(userID=obj).first()
+        PersonalDetails = requestJSONData.get("personalDetails")
+        EmployementDetails = requestJSONData.get("employeeDetails")
+        RBACModelDetails = requestJSONData.get("RBAC")
 
-        returnJsonFile = {
-            "userDetails": {"userName": tempUserObjFetch.userName},
-            "PersonalDetails": {
-                "firstName": tempUserPersonalObjFetch.firstName,
-                "lastName": tempUserPersonalObjFetch.lastName,
-                "email": tempUserPersonalObjFetch.email,
-                "mobile": tempUserPersonalObjFetch.mobile,
-                "address": tempUserPersonalObjFetch.address,
-                "gender": tempUserPersonalObjFetch.gender,
-            },
-            "CompanyDetails": {
-                "companyName": tempUserEmployeesObjFetch.companyName,
-                "designation": tempUserEmployeesObjFetch.designation,
-                "department": tempUserEmployeesObjFetch.department,
-                "workEmail": tempUserEmployeesObjFetch.workEmail,
-                "workPhone": tempUserEmployeesObjFetch.workPhone,
-                "role": tempRoleObjFetch.role,
-            },
-        }
+        UpdateUserPerSonalDetails = PersonalDetailsModel.objects.filter(
+            userID=Objid
+        ).update(**PersonalDetails)
+        UpdateEmployeeDetails = EmployeesModel.objects.filter(userID=Objid).update(
+            **EmployementDetails
+        )
 
-        return returnJsonFile
+        UpdateRBACDetails = RBACModel.objects.filter(userID=Objid).update(
+            **RBACModelDetails
+        )
+
+        # print(UpdateUserDetails)
+
+        return jsonify({"SUCESS": {"message": "User Updated Successfully"}})
+    except:
+        return jsonify({"ERROR": {"message": "Something went wrong"}})
+
+
+@app.route("/user/delete", methods=["DELETE"])
+def deleteUser():
+    if checkLogin() == False:
+        return jsonify({"ERROR": {"message": "Not Logged In"}})
+
+    try:
+        parseRequestParameters = request.values
+        if parseRequestParameters.get("id") is None:
+            return jsonify({"ERROR": {"message": "Invalid Request"}})
+        
+        Objid = ObjectId(parseRequestParameters.get("id"))
+        
+        
+        DeleteUserDetails = UserModel.objects.filter(id=Objid).delete()
+        DeleteUserPersonalDetails =  PersonalDetailsModel.objects.filter(id=Objid).delete()
+        DeleteUserEmployementDetails = EmployeesModel.objects.filter(id=Objid).delete()
+        DeleteUserRABC = RBACModel.objects.filter(id=Objid).delete()
+        
+        logout()
+        return jsonify({"message": "User Deleted Successfully", "id": Objid})
+    
+    except:
+        return jsonify({"ERROR": {"message": "Something went wrong"}})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
